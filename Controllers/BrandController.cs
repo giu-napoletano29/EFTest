5776b52f05199b04c2bb31c7e0f0d5abad029de2
giu-napoletano29 <napoletano@NAPOLETANO-LPT>
2022-01-24 16:46:01 +0100

    Stable

--- Controllers/BrandController.cs
+++ Controllers/BrandController.cs
@@ -36,64 +36,74 @@ namespace TestJuniorDef.Controllers
         [HttpGet("{id}")]
         public IActionResult GetBrandById(int id)
         {
-            var brand = _context.Brands
-                .Where(x => x.Id == id);
+            try
+            {
+                var brand = _context.Brands
+                    .Where(x => x.Id == id);
 
-            var cat = brand
-                .SelectMany(x => x.Products
-                    .SelectMany(y => y.ProductCategory
-                        .Select(z => new CategoryModelAPI
-                        {
-                            Id = z.Category.Id,
-                            CategoryName = z.Category.Name,
+                var cat = brand
+                    .SelectMany(x => x.Products
+                        .SelectMany(y => y.ProductCategory
+                            .Select(z => new CategoryModelAPI
+                            {
+                                Id = z.Category.Id,
+                                CategoryName = z.Category.Name,
                             //TotalProducts = _context.ProductCategories.Where(x => x.CategoryId == z.CategoryId).Count()
                             //TotalProducts = z.Category.ProductCategory.Select(c => c.Product).Where(v => v.BrandId == id).Count(),
                             TotalProducts = x.Products.SelectMany(x => x.ProductCategory.Select(x => x.CategoryId)).Where(x => x == z.CategoryId).Count(),
-                        })))
-                .Distinct()
-                .ToList();
+                            })))
+                    .Distinct()
+                    .ToList();
 
-            var brands = brand
-                    .Select(x => new
-                    {
-                        Id = x.Id,
-                        BrandName = x.BrandName,
-                        TotalProducts = x.Products.Count,
-                        TotalInfoRequest = x.Products.SelectMany(x => x.InfoRequests.Select(x => x.Id)).Count(),                  
-                        Categories = cat,
-                        Products = x.Products.Select(x => new ProductModelAPI
+                var brands = brand
+                        .Select(x => new
                         {
                             Id = x.Id,
-                            ProductName = x.Name,
-                            TotalInfoRequest = x.InfoRequests.Count
-                        })
-                    }).ToList();
-            return Ok(brands);
+                            BrandName = x.BrandName,
+                            TotalProducts = x.Products.Count,
+                            TotalInfoRequest = x.Products.SelectMany(x => x.InfoRequests.Select(x => x.Id)).Count(),
+                            Categories = cat,
+                            Products = x.Products.Select(x => new ProductModelAPI
+                            {
+                                Id = x.Id,
+                                ProductName = x.Name,
+                                TotalInfoRequest = x.InfoRequests.Count
+                            })
+                        }).ToList();
+                return Ok(brands);
+            }
+            catch (System.Exception e)
+            {
+                _logger.LogError(e, e.Message);
+            }
+            return UnprocessableEntity();
         }
 
         [HttpGet("test/{id}")]
         public IActionResult GetBrandByIdTest(int id)
         {
-            var brandQuery =
-                    from brand in _context.Brands
-                    let p = brand.Products
-                    let pc = p.SelectMany(x => x.ProductCategory)
+            try
+            {
+                var brandQuery =
+                        from brand in _context.Brands
+                        let p = brand.Products
+                        let pc = p.SelectMany(x => x.ProductCategory)
                     //let cat = pc.Select(x => x.)
                     where brand.Id == id
-                    select new
-                    {
-                        BrandId = brand.Id,
-                        Brandname = brand.BrandName,
-                        TotalProducts = brand.Products.Count,
-                        TotalReq = brand.Products.SelectMany(x => x.InfoRequests.Select(x => x.Id)).Count(),
-                        Categories = (from c in _context.Categories
-                                      where pc.Select(x => x.CategoryId).Contains(c.Id)
-                                      select new
-                                      {
-                                          Id = c.Id,
-                                          CatName = c.Name,
-                                          Totalprod = p.SelectMany(x => x.ProductCategory.Select(x => x.CategoryId)).Where(x => x == c.Id).Count()
-                                      }).ToList(),
+                        select new
+                        {
+                            BrandId = brand.Id,
+                            Brandname = brand.BrandName,
+                            TotalProducts = brand.Products.Count,
+                            TotalReq = brand.Products.SelectMany(x => x.InfoRequests.Select(x => x.Id)).Count(),
+                            Categories = (from c in _context.Categories
+                                          where pc.Select(x => x.CategoryId).Contains(c.Id)
+                                          select new
+                                          {
+                                              Id = c.Id,
+                                              CatName = c.Name,
+                                              Totalprod = p.SelectMany(x => x.ProductCategory.Select(x => x.CategoryId)).Where(x => x == c.Id).Count()
+                                          }).ToList(),
 
                         //Categories = pc.Select(z => new
                         //{
@@ -103,40 +113,54 @@ namespace TestJuniorDef.Controllers
 
                         //}).ToList(),
                         Products = p.Select(x => new ProductModelAPI
-                        {
-                            Id = x.Id,
-                            ProductName = x.Name,
-                            TotalInfoRequest = x.InfoRequests.Count
+                            {
+                                Id = x.Id,
+                                ProductName = x.Name,
+                                TotalInfoRequest = x.InfoRequests.Count
 
-                        }).ToList(),
-
-
-                    };
+                            }).ToList(),
 
 
+                        };
+                return Ok(brandQuery);
 
-
-            return Ok(brandQuery);
-        }
+            }
+            catch (System.Exception e)
+            {
+                _logger.LogError(e, e.Message);
+            }
+            return UnprocessableEntity();
+}
 
         [HttpGet("page/{size}/{page}")]
         public IActionResult GetbrandPerPage(int size, int page)
         {
-          
-            var brands = _context.Brands.Skip((size*page)-size).Take(size).Select(x => new BrandPagingModelAPI
+            if (size <= 0)
             {
-                BrandName = x.BrandName,
-                Description = x.Description,
-                ProductIds = x.Products.Select(y => y.Id).ToList()
-            }).ToList();
+                return ValidationProblem();
+            }
+            try
+            {
+                var brands = _context.Brands.Skip((size * page) - size).Take(size).Select(x => new BrandPagingModelAPI
+                {
+                    BrandName = x.BrandName,
+                    Description = x.Description,
+                    ProductIds = x.Products.Select(y => y.Id).ToList()
+                }).ToList();
 
-            PagingModelAPI<BrandPagingModelAPI> view = new PagingModelAPI<BrandPagingModelAPI>();
-            view.PageSize = size;
-            view.TotalElements = _context.Brands.Count();
-            view.NumPage = page;
-            view.Elements = brands;
-            
-            return Ok(view);
+                PagingModelAPI<BrandPagingModelAPI> view = new PagingModelAPI<BrandPagingModelAPI>();
+                view.PageSize = size;
+                view.TotalElements = _context.Brands.Count();
+                view.NumPage = page;
+                view.Elements = brands;
+
+                return Ok(view);
+            }
+            catch (System.Exception e)
+            {
+                _logger.LogError(e, e.Message);
+            }
+            return UnprocessableEntity();
         }
 
     }
