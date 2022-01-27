11de3ef16c3823930022d11baed2ae7cd6db0051
Giuseppe Napoletano <giuseppenapoletano29@gmail.com>
2022-01-27 11:01:22 +0100

    Added vue first files

--- TestJuniorDef.FrontEndVue/vue.config.js
+++ TestJuniorDef.FrontEndVue/vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
             cert: fs.readFileSync(certFilePath),
         },
         proxy: {
-            '^/weatherforecast': {
+            '^/test': {
                 target: 'https://localhost:5001/'
             }
         },
