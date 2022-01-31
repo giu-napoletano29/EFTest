1b5934381989f67b4a3dbc7dfc9b519259933cab
Giuseppe Napoletano <giuseppenapoletano29@gmail.com>
2022-01-31 10:35:40 +0100

    Basic layout

--- TestJuniorDef.FrontEndVue/src/App.vue
+++ TestJuniorDef.FrontEndVue/src/App.vue
@@ -1,11 +1,32 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div>
+            <b-navbar type="dark" variant="dark" class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+                <b-navbar-brand href="#" class="navbar-brand col-md-3 col-lg-2 me-0 px-3">Company name</b-navbar-brand>
+            </b-navbar>
+        </div>
+        <div class="container-fluid">
+            <div class="row flex-xl-nowrap2">
+                <!--<div id="nav">
+                <router-link to="/">Home</router-link> |
+                <router-link to="/about">About</router-link>
+            </div>-->
+                <nav id="bd-docs-nav" aria-label="Main navigation" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+                    <div class="position-sticky pt-3">
+                        <h2><router-link to="/">Home</router-link></h2>
+                        <h2><router-link to="/about">About</router-link></h2>
+                        <h2>test</h2>
+                        <h2>test</h2>
+                        <h2>test</h2>
+                        <h2>test</h2>
+                    </div>
+                </nav>
+                <div class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                    <router-view />
+                </div>
+            </div>
+        </div>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style>
--- TestJuniorDef.FrontEndVue/src/main.js
+++ TestJuniorDef.FrontEndVue/src/main.js
@@ -3,12 +3,16 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { NavbarPlugin } from 'bootstrap-vue'
 
 import App from './App.vue'
 import router from './router'
 
+import './assets/styles/main.css'
+
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(NavbarPlugin)
 
 Vue.config.productionTip = false
 
