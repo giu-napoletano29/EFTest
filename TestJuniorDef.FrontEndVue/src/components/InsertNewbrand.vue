c7e9d797ae1a46d7e05d3f53b5b070960a5c5458
Giuseppe Napoletano <giuseppenapoletano29@gmail.com>
2022-02-08 12:28:50 +0100

    Added dynamic components for adding products in new brand

--- TestJuniorDef.FrontEndVue/src/components/InsertNewbrand.vue
+++ TestJuniorDef.FrontEndVue/src/components/InsertNewbrand.vue
@@ -1,16 +1,21 @@
 <template>
     <div>
-        <form v-on:submit.prevent="submitForm">
-            <div class="mb-3">
-                <input type="text" class="form-control" id="productname" placeholder="Nome brand">
-            </div>
-            <div class="mb-3">      
-                <textarea class="form-control" id="descriptio" rows="3" placeholder="Descrizione"></textarea>
-            </div>
-            <div class="mb-3">      
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </div>       
-        </form>
+        <div class="mb-3">
+            <input type="text" class="form-control" placeholder="Nome brand" v-model="brand.BrandName">
+        </div>
+        <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">@</span>
+            <input type="text" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1" v-model="brand.Account.Email">
+        </div>
+        <div class="mb-3">
+            <input type="password" class="form-control" placeholder="Password" v-model="brand.Account.Password">
+        </div>
+        <div class="mb-3">      
+            <textarea class="form-control" rows="3" placeholder="Descrizione" v-model="brand.Description"></textarea>
+        </div>
+        <div class="mb-3"> 
+            <button type="button" class="btn btn-primary" @click="addProd">Aggiungi prodotto</button>
+        </div>
     </div>
 </template>
 
@@ -19,11 +24,31 @@
 export default {
 
     props: {
+        brand:{ 
+            BrandName: String,
+            Description: String,
+            Account:{
+                Email: String,
+                Password: String,
+            }
+        }
 
     },
 
-    methods:{
+    watch: {
+        brand: {
+            deep:true,
+            handler(){
+                console.log("Emit")
+                this.$emit('input', this.brand);
+            }
+        }
+    },
 
+    methods:{
+        addProd(){
+            this.$emit('addprod');
+        }
     },
 }
 </script>
--- TestJuniorDef.FrontEndVue/src/views/InsertNewbrand.vue
+++ TestJuniorDef.FrontEndVue/src/views/InsertNewbrand.vue
@@ -2,9 +2,32 @@
     <div>
         <b-container>
             <Header :name="name"/>
-            <InsertNewbrand
+            <form v-on:submit.prevent="InsertProduct">
+                <InsertNewbrand
+                    :list="list"
+                    :brands="brands"
+                    :loaded="loaded"
+                    :error="error"
+                    :brand="brand"
+                    @input="(newbrand) => {brand = newbrand}"
+                    @addprod="addProd"
+                />
+                <component
+                    v-for="(component, index) in newcomponents"
+                    :key="index"
+                    :is="component"
 
-            />
+                    :list="list"
+                    :brands="brands"
+                    :loaded="loaded"
+                    :error="error"
+                    :product="product"
+                    @input="(newprod) => {product = newprod}"
+                />
+                <div class="mb-3">      
+                    <button type="submit" class="btn btn-primary">Aggiungi brand</button>
+                </div>       
+            </form>
         </b-container>
     </div>
 </template>
@@ -12,8 +35,12 @@
 <script>
     import Header from '@/components/Header.vue'
     import InsertNewbrand from '@/components/InsertNewbrand.vue'
-    //import {Factory} from './../wrappers/Factory'
-    //const BrandsRepo = Factory.get('brands')
+    import InsertNewProduct from '@/components/InsertNewProduct.vue'
+    import {Factory} from './../wrappers/Factory'
+    const CategoriesRepo = Factory.get('categories')
+    const BrandsRepo = Factory.get('brands')
+    const ProductRepo = Factory.get('products')
+    const Comp = InsertNewProduct
 
     export default {
         
@@ -25,13 +52,63 @@
         data(){
             return{
                 name: 'Nuovo Brand',
+                newcomponents: [],
+                list: [],
+                brands: [],
+                loaded: false,
+                error: false,
+                response: '',
 
+                brand:{ 
+                    BrandName: "",
+                    Description: "",
+                    Account:{
+                        Email: "",
+                        Password: "",
+                        AccountType: 1,
+                    },
+                    Products:[]
+                },
+
+                product:{ 
+                    BrandId: "Seleziona brand",
+                    Name: "",
+                    ShortDescription: "",
+                    Price: 0,
+                    Description: "",
+                    ProductCategory: [],
+                }
             }
         },
 
         methods: {
+            async loadElements(){
+                const {data} = await CategoriesRepo.get()
+                this.loaded = true
+                this.list = data;
+            },
 
+            async loadBrands(){
+                const {data} = await BrandsRepo.get()
+                this.loaded = true
+                this.brands = data;
+            },
+
+            InsertProduct(){
+                const resp = ProductRepo.create(this.product)
+                this.response = resp
+                
+            },
+
+            addProd () {
+                this.newcomponents.push(Comp)
+            }
         },
+
+        created() {
+            this.loadElements();
+            this.loadBrands();
+        }
 
     }
 </script>
